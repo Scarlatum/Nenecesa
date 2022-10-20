f2f17cd3343b6908356be0231e65942a24ed3c21
Алексей Скурлатов <Scarlatum@yandex.ru>
2022-10-20 10:39:41 +0800

    [ Typo ]: Rename generic types.

--- source/main.lib.ts
+++ source/main.lib.ts
@@ -1,50 +1,52 @@
-class Optional<DEF, VAL extends DEF | undefined> {
+type Possible<T> = T | undefined;
 
-  static DEFAULT_ERROR = Error('mismatched or undefined type');
+class Optional<Default,Value extends Possible<Default>> {
 
-  #def    : DEF;
-  #value  : VAL | DEF;
-  #err    : Error = Optional.DEFAULT_ERROR;
+    static DEFAULT_ERROR = Error('mismatched or undefined type');
 
-  constructor(defaultValue: DEF, value: VAL, error ?: Error) {
+    #def    : Default;
+    #value  : Value | Default;
+    #err    : Error = Optional.DEFAULT_ERROR;
 
-      this.#def = defaultValue;
-      this.#err = error || Optional.DEFAULT_ERROR;
+    constructor(defaultValue: Default, value: Value, error ?: Error) {
 
-      this.#value = typeof defaultValue === typeof value
-          ? value
-          : defaultValue
+        this.#def = defaultValue;
+        this.#err = error || Optional.DEFAULT_ERROR;
 
-  }
+        this.#value = typeof defaultValue === typeof value
+            ? value
+            : defaultValue
 
-  get some() {
-      return typeof this.#def === typeof this.#value
-          ? this.#value
-          : this.#def
-  }
+    }
 
-  get error() {
-      return this.#def === this.some
-          ? this.#err || Optional.DEFAULT_ERROR
-          : null
-  }
+    get some() {
+        return typeof this.#def === typeof this.#value
+            ? this.#value
+            : this.#def
+    }
+
+    get error() {
+        return this.#def === this.some
+            ? this.#err || Optional.DEFAULT_ERROR
+            : null
+    }
 
 }
 
-class OptionalSet<T> {
+class OptionalSet<Default,Value extends Possible<Default>> {
 
-  #opt: Array<Optional<T,T>> = Array(0);
+    #opt: Array<Optional<Default,Value>> = Array(0);
 
-  constructor(opts: Array<Optional<T,T>>) {
-      this.#opt = opts;
-  }
+    constructor(opts: Array<Optional<Default,Value>>) {
+        this.#opt = opts;
+    }
 
-  get values() {
-      return this.#opt.filter(({ error }) => !error ).map(({ some }) => some)
-  }
+    get values() {
+        return this.#opt.filter(({ error }) => !error ).map(({ some }) => some)
+    }
 
-  get errors() {
-      return this.#opt.filter(({ error }) => error ).map(({ error }) => error)
-  }
+    get errors() {
+        return this.#opt.filter(({ error }) => error ).map(({ error }) => error)
+    }
 
 }
